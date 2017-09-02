08397daddde971bbae27b54e7bfdc35f874df051
mordicool <mordicool@gmail.com>
2017-09-02 21:37:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 var express = require('express'),
     fs      = require('fs'),
     app     = express(),
-    eps     = require('ejs'),
     morgan  = require('morgan');
     
 Object.assign=require('object-assign')
