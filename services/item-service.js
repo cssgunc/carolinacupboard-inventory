0f545178eb42234b9aafc268057661d06a7a34e7
Richard Le <richardle798@gmail.com>
2019-04-04 18:52:44 -0400

    implemented add and remove item

--- services/item-service.js
+++ services/item-service.js
@@ -1,7 +1,9 @@
-const   Item = require("../db/sequelize").item,
+const   Item = require("../db/sequelize").items,
+        Transaction = require("../db/sequelize").transactions,
         Sequelize = require("sequelize"),
         BadRequestException = require("../exceptions/bad-request-exception"),
-        InternalErrorException = require("../exceptions/internal-error-exception");
+        InternalErrorException = require("../exceptions/internal-error-exception"),
+        CarolinaCupboardException = require("../exceptions/carolina-cupboard-exception");
 
 exports.createItem = async function (name, barcode, description) {
     try {
@@ -25,9 +27,56 @@ exports.createItem = async function (name, barcode, description) {
 
 exports.getItems = async function () {
     try {
-        let items = await Attribute.findAll();
+        let items = await Item.findAll();
         return items;
     } catch (e) {
         throw new InternalErrorException("A problem occurred when retrieving items",e);
+    }
+}
+
+exports.getItem = async function (itemId) {
+    try {
+        let item = await Item.findOne({ where: { id: itemId } });
+        if (!item) {
+            throw new BadRequestException("The item could not be retrieved.");
+        }
+        return item;
+    } catch (e) {
+        if(e instanceof CarolinaCupboardException) {
+            throw e;
+        }
+
+        throw new InternalErrorException("A problem occurred when retrieving the item",e);
+    }
+}
+
+exports.addItems = async function (itemId, quantity, onyen, volunteerId) {
+    await this.createTransaction(itemId, quantity, onyen, volunteerId);
+}
+
+exports.removeItems = async function (itemId, quantity, onyen, volunteerId) {
+    await this.createTransaction(itemId, -quantity, onyen, volunteerId);
+}
+
+exports.createTransaction = async function (itemId, quantity, onyen, volunteerId) {
+    let item = await this.getItem(itemId);
+    item.count += quantity;
+
+    if(item.count < 0) {
+        throw new BadRequestException("The amount requested is larger than the quantity in the system");
+    }
+
+    try {
+        let transaction = await Transaction.build({
+            item_id: itemId,
+            count: -quantity,
+            onyen: onyen,
+            volunteer_id: volunteerId,
+            status: 'complete'
+        });
+        await item.save();
+        await transaction.save();
+    } catch (e) {
+        throw new InternalErrorException("A problem occurred when adding the transaction",e);
     }
 }
