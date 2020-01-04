914b0b21351bde2d7f286d3f6db6c4a5b068027a
Austin Wang <austin.wang0311@gmail.com>
2020-01-04 01:52:58 -0500

    Add datatables, make indent consistent within files, remove form tags

--- static/js/preorders.js
+++ static/js/preorders.js
@@ -1,29 +1,34 @@
-$('#completeModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget); // Button that triggered the modal
-    var id = button.data('id');
-    var name = button.data('name');
-    var onyen = button.data('onyen');
-    var count = button.data('count');
-    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-    var modal = $(this);
-    modal.find('#completeModalId').val(id);
-    modal.find('#completeModalName').val(name);
-    modal.find('#completeModalOnyen').val(onyen);
-    modal.find('#completeModalCount').val(count);
-});
+$(document).ready(function () {
+    $('#preordersTable').DataTable();
+    $('.dataTables_length').addClass('bs-select');
+    
+    $('#completeModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget); // Button that triggered the modal
+        var id = button.data('id');
+        var name = button.data('name');
+        var onyen = button.data('onyen');
+        var count = button.data('count');
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this);
+        modal.find('#completeModalId').val(id);
+        modal.find('#completeModalName').val(name);
+        modal.find('#completeModalOnyen').val(onyen);
+        modal.find('#completeModalCount').val(count);
+    });
 
-$('#cancelModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget); // Button that triggered the modal
-    var id = button.data('id');
-    var name = button.data('name');
-    var onyen = button.data('onyen');
-    var count = button.data('count');
-    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-    var modal = $(this);
-    modal.find('#cancelModalId').val(id);
-    modal.find('#cancelModalName').val(name);
-    modal.find('#cancelModalOnyen').val(onyen);
-    modal.find('#cancelModalCount').val(count);
+    $('#cancelModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget); // Button that triggered the modal
+        var id = button.data('id');
+        var name = button.data('name');
+        var onyen = button.data('onyen');
+        var count = button.data('count');
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this);
+        modal.find('#cancelModalId').val(id);
+        modal.find('#cancelModalName').val(name);
+        modal.find('#cancelModalOnyen').val(onyen);
+        modal.find('#cancelModalCount').val(count);
+    });
 });
