1a6e5eb6f2cf8cbb96947d9a5ff09447d3584c15
Austin Wang <austin.wang0311@gmail.com>
2020-06-29 17:50:29 -0400

    Add index route test, remove deleted scan import

--- controllers/index.js
+++ controllers/index.js
@@ -4,14 +4,12 @@ let express     = require("express"),
     entry       = require("./entry"),
     history     = require("./history"),
     item        = require("./items"),
-    preorders   = require("./preorders"),
-    scan        = require("./scan");
+    preorders   = require("./preorders");
     
 router.use("/admin", admin);
 router.use("/entry", entry);
 router.use("/history", history);
 router.use("/items", item);
 router.use("/preorders", preorders);
-router.use("/scan", scan);
 
 module.exports = router;
