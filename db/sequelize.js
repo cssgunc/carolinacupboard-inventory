bb9d897b7dd8b96ea9ac6a6424895673f4eb4fa2
Austin Wang <austin.wang0311@gmail.com>
2020-07-02 05:26:15 -0400

    Decrease pool size from 3 to 2

--- db/sequelize.js
+++ db/sequelize.js
@@ -11,7 +11,7 @@ if (!process.env.DATABASE_URL) {
 let options = {
     dialect: 'postgres',
     pool: {
-        max: 3,
+        max: 2,
         min: 0,
         acquire: 30000,
         idle: 10000
