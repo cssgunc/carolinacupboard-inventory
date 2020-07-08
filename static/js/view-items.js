2fa7b37f84694eb11aaab0c990f1f537253072e9
Austin Wang <austin.wang0311@gmail.com>
2020-07-08 02:36:14 -0400

    Change reserve to add to cart, create cart in local storage

--- static/js/view-items.js
+++ static/js/view-items.js
@@ -3,19 +3,42 @@ $(document).ready(function () {
         'order': [[2, 'desc']]
     });
 
-    $('#reserveModal').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget); // Button that triggered the modal
-        var id = button.data('id');
-        var name = button.data('name');
-        var barcode = button.data('barcode');
-        var maxCount = button.data('count');
-        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-        var modal = $(this);
-        modal.find('#reserveModalId').val(id);
-        modal.find('#reserveModalName').val(name);
-        modal.find('#reserveModalBarcode').val(barcode);
-        modal.find('#reserveModalQuantity').attr('max', maxCount);
-        modal.find('#reserveModalQuantity').val(1);
+    $('#addToCartButton').on('click', function (event) {
+        const button = $(event.target); // Button that triggered the modal
+        const id = button.data('id');
+        const name = button.data('name');
+        const barcode = button.data('barcode');
+        const count = button.data('count');
+        const addToCartQuantity = $('#cartQuantity').val();
+
+        if (addToCartQuantity <= count) {
+            
+            const newItem = {
+                id: id,
+                name: name,
+                barcode: barcode,
+                quantity: addToCartQuantity
+            }
+
+            let cart = localStorage.getItem('cart');
+            if (cart) {
+                cart = JSON.parse(cart);
+                let found = false;
+                for (let i = 0; i < cart.length; i++) {
+                    if (cart[i].id === id) {
+                        cart[i].quantity = Math.min(count, parseInt(cart[i].quantity) + parseInt(addToCartQuantity));
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) cart.push(newItem);
+                localStorage.setItem('cart', JSON.stringify(cart));
+            } else {
+                console.log('dont exists');
+                localStorage.setItem('cart', JSON.stringify([newItem]));
+            }
+
+            console.log(localStorage.getItem('cart'));
+        }
     });
 });
