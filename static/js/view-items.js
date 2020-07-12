874b17cf673f547e05839f9eced958230e37bac0
Austin Wang <austin.wang0311@gmail.com>
2020-07-12 17:46:25 -0400

    Changed add to cart to support multiple items

--- static/js/view-items.js
+++ static/js/view-items.js
@@ -5,100 +5,107 @@ $(document).ready(function () {
         'order': [[2, 'desc']]
     });
 
-    $('#addToCartButton').on('click', function (event) {
-        const button = $(event.target); // Button that triggered the modal
-        
-        // Pulls data values attached to button
-        const id = button.data('id');
-        const name = button.data('name');
-        const barcode = button.data('barcode');
-        const count = button.data('count');
-        const description = button.data('description');
-        const addToCartQuantity = $('#cartQuantity').val();
+    let buttons = document.querySelectorAll('.addToCartButton');
+    for(let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function (event) {
+            const button = $(event.target); // Button that triggered the modal
+            
+            // Pulls data values attached to button
+            const id = button.data('id');
+            const name = button.data('name');
+            const barcode = button.data('barcode');
+            const count = button.data('count');
+            const description = button.data('description');
+            let parent = button.parent().parent();
+            const addToCartQuantity = parent.children('.cartQuantity').val();
+            console.log(addToCartQuantity);
 
-        // Create toast alert
-        let toast = document.createElement('div');
-        toast.className = 'toast';
-        toast.setAttribute('role', 'status');
-        toast.setAttribute('aria-live', 'polite');
-        toast.setAttribute('aria-atomic', 'true');
-        toast.setAttribute('data-autohide', 'true');
-        toast.setAttribute('data-delay', '5000');
-        
-        let toastHeader = document.createElement('div');
-        toastHeader.className = 'toast-header';
+            // Create toast alert
+            let toast = document.createElement('div');
+            toast.className = 'toast';
+            toast.setAttribute('role', 'status');
+            toast.setAttribute('aria-live', 'polite');
+            toast.setAttribute('aria-atomic', 'true');
+            toast.setAttribute('data-autohide', 'true');
+            toast.setAttribute('data-delay', '5000');
+            
+            let toastHeader = document.createElement('div');
+            toastHeader.className = 'toast-header';
 
-        let toastHeaderText = document.createElement('strong');
-        toastHeaderText.className = 'mr-auto';
+            let toastHeaderText = document.createElement('strong');
+            toastHeaderText.className = 'mr-auto';
 
-        let toastClose = document.createElement('button');
-        toastClose.className = 'ml-2 mb-1 close';
-        toastClose.setAttribute('type', 'button');
-        toastClose.setAttribute('data-dismiss', 'toast');
-        toastClose.setAttribute('aria-label', 'Close');
+            let toastClose = document.createElement('button');
+            toastClose.className = 'ml-2 mb-1 close';
+            toastClose.setAttribute('type', 'button');
+            toastClose.setAttribute('data-dismiss', 'toast');
+            toastClose.setAttribute('aria-label', 'Close');
 
-        let toastCloseIcon = document.createElement('span');
-        toastCloseIcon.setAttribute('aria-hidden', 'true');
-        toastCloseIcon.innerHTML = '&times;';
+            let toastCloseIcon = document.createElement('span');
+            toastCloseIcon.setAttribute('aria-hidden', 'true');
+            toastCloseIcon.innerHTML = '&times;';
 
-        let toastBody = document.createElement('div');
-        toastBody.className = 'toast-body';
+            let toastBody = document.createElement('div');
+            toastBody.className = 'toast-body';
 
-        toastHeader.appendChild(toastHeaderText);
-        toastClose.appendChild(toastCloseIcon);
-        toastHeader.appendChild(toastClose);
-        toast.appendChild(toastHeader);
-        toast.appendChild(toastBody);
-        document.getElementById('toast-pos').appendChild(toast);
+            toastHeader.appendChild(toastHeaderText);
+            toastClose.appendChild(toastCloseIcon);
+            toastHeader.appendChild(toastClose);
+            toast.appendChild(toastHeader);
+            toast.appendChild(toastBody);
+            document.getElementById('toast-pos').appendChild(toast);
 
-        if (addToCartQuantity <= count) {
-            const newItem = {
-                id: id,
-                name: name,
-                barcode: barcode,
-                quantity: addToCartQuantity,
-                description: description
-            }
-
-            let cart = localStorage.getItem('cart');
-
-            if (cart) {
-                cart = JSON.parse(cart);
-                let found = false;
-                // Adds to existing amount if item is already in cart
-                for (let i = 0; i < cart.length; i++) {
-                    if (cart[i].id === id) {
-                        // Cannot add more than currently exist in inventory
-                        if (count === parseInt(cart[i].quantity)) {
-                            toastHeaderText.appendChild(document.createTextNode('Error'));
-                            toastBody.appendChild(document.createTextNode('There is not enough ' + name + ' in stock'));
-                        } else {
-                            cart[i].quantity = Math.min(count, parseInt(cart[i].quantity) + parseInt(addToCartQuantity));
-                            console.log('ADDED');
-                            toastHeaderText.appendChild(document.createTextNode('Success'));
-                            toastBody.appendChild(document.createTextNode(name + ' added to cart, qty: ' + addToCartQuantity));
-                        }
-                        found = true;
-                        break;
-                    }
+            if (addToCartQuantity <= count) {
+                const newItem = {
+                    id: id,
+                    name: name,
+                    barcode: barcode,
+                    quantity: addToCartQuantity,
+                    description: description
                 }
-                // If not yet in cart, push a new item
-                if (!found) cart.push(newItem);
-                localStorage.setItem('cart', JSON.stringify(cart));
-            } else {
-                localStorage.setItem('cart', JSON.stringify([newItem]));
-                toastHeaderText.appendChild(document.createTextNode('Success'));
-                toastBody.appendChild(document.createTextNode(name + ' added to cart, qty: ' + addToCartQuantity));
-            }
-        } else {
-            toastHeaderText.appendChild(document.createTextNode('Error'));
-            toastBody.appendChild(document.createTextNode('There is not enough ' + name + ' in stock'));
-        }
 
-        $('.toast').toast('show');
-        $('.toast').on('hidden.bs.toast', (e) => {
-            console.log(e);
-            e.target.outerHTML = '';
+                let cart = localStorage.getItem('cart');
+
+                if (cart) {
+                    cart = JSON.parse(cart);
+                    let found = false;
+                    // Adds to existing amount if item is already in cart
+                    for (let i = 0; i < cart.length; i++) {
+                        if (cart[i].id === id) {
+                            // Cannot add more than currently exist in inventory
+                            if (count === parseInt(cart[i].quantity)) {
+                                toastHeaderText.appendChild(document.createTextNode('Error'));
+                                toastBody.appendChild(document.createTextNode('There is not enough ' + name + ' in stock'));
+                            } else {
+                                cart[i].quantity = Math.min(count, parseInt(cart[i].quantity) + parseInt(addToCartQuantity));
+                                toastHeaderText.appendChild(document.createTextNode('Success'));
+                                toastBody.appendChild(document.createTextNode(name + ' added to cart, qty: ' + addToCartQuantity));
+                            }
+                            found = true;
+                            break;
+                        }
+                    }
+                    // If not yet in cart, push a new item
+                    if (!found) {
+                        cart.push(newItem);
+                        toastHeaderText.appendChild(document.createTextNode('Success'));
+                        toastBody.appendChild(document.createTextNode(name + ' added to cart, qty: ' + addToCartQuantity));
+                    }
+                    localStorage.setItem('cart', JSON.stringify(cart));
+                } else {
+                    localStorage.setItem('cart', JSON.stringify([newItem]));
+                    toastHeaderText.appendChild(document.createTextNode('Success'));
+                    toastBody.appendChild(document.createTextNode(name + ' added to cart, qty: ' + addToCartQuantity));
+                }
+            } else {
+                toastHeaderText.appendChild(document.createTextNode('Error'));
+                toastBody.appendChild(document.createTextNode('There is not enough ' + name + ' in stock'));
+            }
+
+            $('.toast').toast('show');
+            $('.toast').on('hidden.bs.toast', (e) => {
+                e.target.outerHTML = '';
+            });
         });
-    });
+    }
 });
