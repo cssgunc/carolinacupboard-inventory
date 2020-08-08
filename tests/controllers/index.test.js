f2349e8be0f2a25a4ee8978678b4f11c0345e76a
Austin Wang <austin.wang0311@gmail.com>
2020-08-08 01:42:11 -0400

    Move test dev deps to deps for openshift

--- tests/controllers/index.test.js
+++ tests/controllers/index.test.js
@@ -6,14 +6,11 @@ const supertest = require('supertest'),
 
 require('dotenv').config();
 
-before(async () => {
-    await dbUtil.dropTables(false);
-    await dbUtil.createTables(false);
-    await dbUtil.initAdmin(false);
-    await dbUtil.initTestUsers(false);
-});
-
 describe('Index routes - GET pages', () => {
+    before(async () => {
+        await dbUtil.preTestSetup();
+    });
+    
     describe('GET / - home page', () => {
         it('expect success HTTP 200 status', (done) => {
             supertest(app).get('/')
